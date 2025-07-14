7eaac9172a76bd446df980e74d3ab4e171e64a30
robinson732 <kimanirobinson336@gmail.com>
2025-07-14 14:01:29 +0300

    final

---
+++ src/components/Links.js
@@ -0,0 +1,10 @@
+function Links(props){
+  return (
+    <>
+    <h3>Links</h3>
+    <a href="github.com/liza">GitHub</a>
+    <a href="linkedin.com/in/liza">LinkedIn</a>
+    </>
+  )
+}
+export default Links;
